cd1462beb345cc50b51d10a398437de0d9690c33
Unknown <vinc.claveau@gmail.com>
2018-11-09 12:36:45 +0100

    Suppression User et ajout des Tickets en session

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is a part of AirHotel
+ *
+ * TicketController.php
+ *
+ * @author      Vincent CLAVEAU <vinc.claveau@gmail.com>
+ * @copyright   2018 Vincent CLAVEAU
+ * @date        08/11/2018
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+/**
+ * @Route("/ticket", name = "ticket_")
+ */
+class TicketController extends AbstractController
+{
+    /**
+     * @Route("/login", name = "login")
+     */
+    public function login(Request $request, SessionInterface $session, Environment $twig)
+    {
+        $ticket = $request->request->get('ticket');
+
+        if($ticket !== null)
+        {
+            $session->set('ticket', $ticket);
+            return $this->redirectToRoute('ticket_show');
+        }
+
+        return $this->redirectToRoute('app_index');
+    }
+
+    /**
+     * @Route("/show", name = "show")
+     */
+    public function show(SessionInterface $session, Environment $twig)
+    {
+        if($session->get('ticket') === null)
+        {
+            throw new NotFoundHttpException("No ticket for your session");
+        }
+
+        return new Response($twig->render('ticket/show.html.twig'));
+    }
+}
