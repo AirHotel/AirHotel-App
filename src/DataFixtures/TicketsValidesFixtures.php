5cbcea5d465f05c74ebd52e0303b7c1946be9042
Unknown <vinc.claveau@gmail.com>
2018-11-09 12:43:02 +0100

    changement TicketsValides

--- src/DataFixtures/TicketsValidesFixtures.php
+++ src/DataFixtures/TicketsValidesFixtures.php
@@ -16,12 +16,12 @@ class TicketsValidesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        foreach ($this->getData() as [$numero_ticket])
+        foreach ($this->getData() as [$id])
         {
-            $rt = new TicketsValides();
-            $rt->setNumeroTicket($numero_ticket);
+            $ticket = new TicketsValides();
+            $ticket->setId($id);
 
-            $manager->persist($rt);
+            $manager->persist($ticket);
         }
 
         $manager->flush();
@@ -30,12 +30,16 @@ class TicketsValidesFixtures extends Fixture
     private function getData(): array
     {
         return [
-            ['ABCDEFGH'],
-            ['BCDEFGHI'],
-            ['CDEFGHIJ'],
-            ['DEFGHIJK'],
-            ['EFGHIJKL'],
-            ['FGHIJKLM'],
+            ['71b7b09d-f94c-498a-9eb8-e300f89c9c48'],
+            ['77e05085-a706-40e4-9b90-705210ce1f2e'],
+            ['dbfe789e-3fe6-4b3d-8b88-d4f7744927a8'],
+            ['8b6038cd-f321-488d-8862-ce53487d976c'],
+            ['1c9d7e56-5bac-4e65-8911-f8b4cc9d8e9f'],
+            ['4aff41a3-3efb-4410-bab1-91c045c6cb29'],
+            ['a9b82693-46d9-4487-874a-aa34a2ad462f'],
+            ['7d2b796b-205f-4a26-a51b-6ffc85941d43'],
+            ['77487568-fec4-4c41-9c1c-9dd2372d9608'],
+            ['d8c68a8d-4276-4a6d-bb80-c98aa1623412'],
         ];
     }
 }
